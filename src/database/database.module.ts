282d4208b711f09da195d522e54383e32143eb20
Tery <terrychinen55@gmail.com>
2021-06-09 08:01:59 -0500

    First commit with the connection to the database

---
+++ src/database/database.module.ts
@@ -0,0 +1,28 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: [`.env.${process.env.NODE_ENV}`],
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get('DB_HOST'),
+        port: configService.get('DB_PORT'),
+        username: configService.get('DB_USERNAME'),
+        password: configService.get('DB_PASSWORD'),
+        database: configService.get('DB_DATABASE'),
+        autoLoadEntities: configService.get('DB_'),
+        synchronize: configService.get('DB_AUTO_LOAD_ENTITY'),
+        entities: [configService.get('DB_ENTITIES')],
+        migrations: [configService.get('DB_MIGRATIONS')],
+      }),
+    }),
+  ],
+})
+export class DatabaseModule {}
