fc6731c011da6a5c9f41d38727c06e1affcc525d
terrychinen <terrychinen55@gmail.com>
2021-06-15 06:07:27 -0500

    Adding hospital module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,15 @@
 import { Module } from '@nestjs/common';
 import { DatabaseModule } from './database/database.module';
 import { AuthModule } from './features/auth/auth.module';
+import { HospitalModule } from './features/hospital/hospital.module';
 
 @Module({
-  imports: [DatabaseModule, AuthModule],
+  imports: [
+    DatabaseModule,
+    AuthModule,
+    HospitalModule
+  ],
   controllers: [],
   providers: [],
 })
-export class AppModule {}
+export class AppModule { }
