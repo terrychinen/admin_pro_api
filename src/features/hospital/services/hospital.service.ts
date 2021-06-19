f4efdcf7fd45890e90cf40be154c44463aea8897
terrychinen <terrychinen55@gmail.com>
2021-06-18 22:04:19 -0500

    CRUD of hospital service completed

--- src/features/hospital/services/hospital.service.ts
+++ src/features/hospital/services/hospital.service.ts
@@ -1,45 +1,100 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, InternalServerErrorException, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { HospitalRepository } from '../repositories/hospital.repository';
 import { CreateHospitalDto } from '../dtos/create-hospital.dto';
+import { User } from '../../auth/entities/user.entity';
+import { Hospital } from '../entities/hospital.entity';
+import { PaginationDto } from '../../../global/dtos/pagination.dto';
+import { UpdateHospitalDto } from '../dtos/update-hospital.dto';
+import { IDeleteResult } from '../interfaces/delete-result.inteface';
 
 @Injectable()
 export class HospitalService {
-    
+
     constructor(
         @InjectRepository(HospitalRepository)
         private readonly hospitalRepository: HospitalRepository
-    ) {}
+    ) { }
 
-    getAllHospitals() {
-        return this.hospitalRepository.find();
+    async getAllHospitals(paginationDto: PaginationDto): Promise<Hospital[]> {
+        const { skip, take } = paginationDto;
+
+        const hospitals: Hospital[] = await this.hospitalRepository
+            .createQueryBuilder('hospital')
+            .select(['hospital.id', 'hospital.name', 'user.id', 'user.name'])
+            .innerJoin('hospital.user', 'user')
+            .skip(skip)
+            .take(take)
+            .getMany().catch((err) => {
+                throw new InternalServerErrorException(err);
+            });
+
+        return hospitals;
     }
 
-    getHospitalByID(id: string) {
-        return {
-            ok: true,
-            message: `Get hospital with id ${id}`
+    async getHospitalByID(hospitalID: string): Promise<Hospital> {
+        const findHospital: Hospital = await this.hospitalRepository.findOne({ id: hospitalID });
+
+        if (!findHospital) {
+            throw new NotFoundException('Hospital ID not found');
         }
+
+        const hospital: Hospital = await this.hospitalRepository
+            .createQueryBuilder('hospital')
+            .select(['hospital.id', 'hospital.name', 'user.id', 'user.name'])
+            .innerJoin('hospital.user', 'user')
+            .where('hospital.id = :id', { id: hospitalID })
+            .getOne()
+            .catch((err) => {
+                throw new InternalServerErrorException(err);
+            });
+
+        return hospital;
     }
 
-    createHospital(createHospitalDto: CreateHospitalDto) {       
-        return {
-            ok: true,
-            message: 'Create hospital'
-        }
+    createHospital(
+        user: User,
+        createHospitalDto: CreateHospitalDto
+    ): Promise<Hospital> {
+        return this.hospitalRepository.createHospital(user, createHospitalDto);
     }
 
-    updateHospital() {
-        return {
-            ok: true,
-            message: 'Update hospital'
-        }
+    async updateHospital(
+        hospitalID: string,
+        updateHospitalDto: UpdateHospitalDto
+    ): Promise<Hospital> {
+        const { name } = updateHospitalDto;
+
+        const hospital: Hospital = await this.getHospitalByID(hospitalID);
+        hospital.name = name;
+
+        return this.hospitalRepository.save({
+            id: hospitalID,
+            hospital
+        }).catch((err) => {
+            throw new InternalServerErrorException(err);
+        });
     }
 
-    deleteHospital(id: string) {
-        return {
-            ok: true,
-            message: `Delete hospital with id ${id}`
-        }
+    deleteHospital(id: string): Promise<IDeleteResult> {
+        return this.hospitalRepository.delete(id).then((res) => {
+            let deleteResult: IDeleteResult;
+
+            if (res.affected >= 1) {
+                return deleteResult = {
+                    ok: true,
+                    message: 'Hospital deleted'
+                };
+            }
+
+            return deleteResult = {
+                ok: false,
+                message: 'Ups, something went wrong'
+            };
+
+
+        }).catch((err) => {
+            throw new InternalServerErrorException(err);
+        });
     }
 }
