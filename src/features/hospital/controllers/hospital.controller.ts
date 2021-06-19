0e34c2b219e3ca8ea9505ac9e20871a3dabedf54
terrychinen <terrychinen55@gmail.com>
2021-06-18 22:04:54 -0500

    CRUD of hospital controller completed

--- src/features/hospital/controllers/hospital.controller.ts
+++ src/features/hospital/controllers/hospital.controller.ts
@@ -1,35 +1,57 @@
-import { Body, Controller, Delete, Get, Param, Patch, Post, UseGuards } from '@nestjs/common';
+import {
+    Body, Controller,
+    Delete, Get, Param,
+    ParseUUIDPipe,
+    Patch, Post, Query, UseGuards
+} from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 import { HospitalService } from '../services/hospital.service';
 import { CreateHospitalDto } from '../dtos/create-hospital.dto';
+import { AuthJwt } from '../../auth/decorators/jwt-auth.decorator';
+import { PaginationDto } from '../../../global/dtos/pagination.dto';
+import { UpdateHospitalDto } from '../dtos/update-hospital.dto';
+import { Hospital } from '../entities/hospital.entity';
+import { IDeleteResult } from '../interfaces/delete-result.inteface';
 
 @UseGuards(AuthGuard('jwt'))
 @Controller('hospital')
 export class HospitalController {
-    constructor(private readonly hospitalService: HospitalService) {}
+    constructor(private readonly hospitalService: HospitalService) { }
 
     @Get()
-    getAllHospitals() {
-        return this.hospitalService.getAllHospitals();    
+    getAllHospitals(
+        @Query() paginationDto: PaginationDto
+    ): Promise<Hospital[]> {
+        return this.hospitalService.getAllHospitals(paginationDto);
     }
 
     @Get('/:id')
-    getHospitalByID(@Param('id') id: string) {
-        return this.hospitalService.getHospitalByID(id);
+    getHospitalByID(
+        @Param('id', ParseUUIDPipe) hospitalID: string
+    ): Promise<Hospital> {
+        return this.hospitalService.getHospitalByID(hospitalID);
     }
 
     @Post()
-    createHospital(@Body() createHospitalDto: CreateHospitalDto) {
-        return this.hospitalService.createHospital(createHospitalDto);
+    createHospital(
+        @AuthJwt() user,
+        @Body() createHospitalDto: CreateHospitalDto
+    ): Promise<Hospital> {
+        return this.hospitalService.createHospital(user, createHospitalDto);
     }
 
-    @Patch()
-    updateHospital() {
-        return this.hospitalService.updateHospital();
+    @Patch('/:id')
+    updateHospital(
+        @Param('id', ParseUUIDPipe) hospitalID: string,
+        @Body() updateHospitalDto: UpdateHospitalDto
+    ): Promise<Hospital> {
+        return this.hospitalService.updateHospital(hospitalID, updateHospitalDto);
     }
 
     @Delete('/:id')
-    deleteHospital(@Param('id') id: string) {
+    deleteHospital(
+        @Param('id', ParseUUIDPipe) id: string
+    ): Promise<IDeleteResult> {
         return this.hospitalService.deleteHospital(id);
     }
 }
